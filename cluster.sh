50030043440b0425ba2bb8da8d1a0cc4f2af9825
linxin98 <844340004@qq.com>
2021-06-18 20:55:31 +0800

    Upload cluster shell.

---
+++ cluster.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for dataset in "cit-HepTh" "cit-HepPh"
+do
+    for feature_type in 0 1 2
+    do
+        for label_type in 1 2
+        do
+            save_path="./results/cluster/${dataset}-${feature_type}-${label_type}.csv"
+            python cluster/cluster.py -d $dataset -f $feature_type -l $label_type -sp $save_path
+            echo $save_path
+        done
+    done
+done
